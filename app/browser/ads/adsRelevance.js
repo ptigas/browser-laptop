eb51be9ea2963234a2f2ec93a80060ddac20b233
Panos <ptigas@brave.com>
2018-07-25 20:54:06 +0100

    added scoring and feature extraction

--- app/browser/ads/adsRelevance.js
+++ app/browser/ads/adsRelevance.js
@@ -4,6 +4,8 @@
 
  'use strict'
  const um = require('@brave-intl/bat-usermodel')
+ const userFeatures = require('../../common/constants/userFeatures')
+ //const adFeatures = require('../../common/constants/adFeatures')
 
  const notificationScore = (ads) => {
     const notificationWeights = um.getNotificationsModel()
@@ -19,15 +21,23 @@
     return um.notificationScore(featureVector, notificationWeights);
  }
 
- const scoreAdsRelevance = (ads) => {
-     const scores = {}
-     const keys = Object.keys(ads)
-     keys.forEach(key => {
-         scores[key] = 1.0
-     })
+const scoreAdsRelevance = (adsFeatures) => {
+    const scores = []
+    const model = um.getAdsRelevanceModel()
+
+    for (let i = 0; i < adsFeatures.length; i++ ) {
+        const score = um.logisticRegression(adsFeatures[i], model)
+        if (score > 0.5) {
+            console.log("YEY: " + score)
+            console.log(adsFeatures[i])
+            scores.push(score)
+        } else {
+            scores.push(0)
+        }
+    }
  
-     return scores
- }
+    return scores
+}
  
  const normalize = (dic) => {
      let sum = 0.0
@@ -47,7 +57,7 @@
     return dic
 }
 
-const sampleAd = (ads, scores) => {
+const sampleAd = (scores) => {
     const normalizedScores = normalize(scores)
     const keys = Object.keys(scores)
 
@@ -64,9 +74,93 @@ const sampleAd = (ads, scores) => {
     return -1;
 }
 
-const extractAdsFeatures = (ads, userFeatures) => {
+const updateState = (state, key, callback) => {
+    state = validateState(state)
+    return callback(state.getIn(key))
+}
 
-    return ads
+const vector2topic = (pageScore) => {
+    const priorData = um.getPriorDataSync()
+    const catNames = priorData.names
+  
+    const immediateMax = um.vectorIndexOfMax(pageScore)
+    const immediateWinner = catNames[immediateMax]
+    return immediateWinner
+}
+
+const categoryMatchScore = (adCategory, interest, entropy) => {
+    // if entropy is very high then return no match
+    if (entropy >= 0.8) {
+        return 0.0
+    }
+
+    // the score depends on the level that the interest match with the category
+    // if the interest is auto-cars and the ad is auto-cars it gets score 1
+    // however if the ad is auto then it gets 0.5 because 
+    // it matched only the high level category
+    const hierarchy = interest.split('-')
+    let score = 1.0
+    for (let level=0; level < hierarchy.length; level++) {
+        const topic = hierarchy[level]
+        if (topic == adCategory) {
+            break
+        }
+        score -= (1.0 / hierarchy.length)
+    }
+    
+    return score
+}
+
+const extractAdFeature = (ad, userFeatureVector) => {
+    let featureVector = new Map()
+
+    featureVector.set(
+        'intent_category_match', 
+        categoryMatchScore(
+            ad['category'], 
+            vector2topic(userFeatureVector.get(userFeatures.INTENT)),
+            userFeatureVector.get(userFeatures.INTENT_ENTROPY)
+        )
+    )
+
+    featureVector.set(
+        'short_term_category_match', 
+        categoryMatchScore(
+            ad['category'], 
+            vector2topic(userFeatureVector.get(userFeatures.SHORT_INTEREST)),
+            userFeatureVector.get(userFeatures.SHORT_INTEREST_ENTROPY)
+        )
+    )
+
+    featureVector.set(
+        'long_term_category_match', 
+        categoryMatchScore(
+            ad['category'], 
+            vector2topic(userFeatureVector.get(userFeatures.LONG_INTEREST)),
+            userFeatureVector.get(userFeatures.LONG_INTEREST_ENTROPY)
+        )
+    )
+
+    return featureVector
+}
+
+const extractAdsFeatures = (ads, userFeatureVector) => {
+
+    let features = []
+    
+    console.log("FEATURES START")
+    
+    console.log("Intent: " + vector2topic(userFeatureVector.get(userFeatures.INTENT)) + " entropy: " + userFeatureVector.get(userFeatures.INTENT_ENTROPY))
+    console.log("Short: " + vector2topic(userFeatureVector.get(userFeatures.SHORT_INTEREST)) + " entropy: " + userFeatureVector.get(userFeatures.SHORT_INTEREST_ENTROPY))
+    console.log("Long: " + vector2topic(userFeatureVector.get(userFeatures.LONG_INTEREST)) + " entropy: " + userFeatureVector.get(userFeatures.LONG_INTEREST_ENTROPY))
+    
+    for (let id in ads) {
+        features.push(extractAdFeature(ads[id], userFeatureVector))
+    }
+
+    console.log("FEATURES END")
+
+    return features
 }
 
 module.exports = {
